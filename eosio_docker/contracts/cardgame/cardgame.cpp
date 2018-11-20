2cc520b148751f2e8c138a3cb794ba6b82159578
Andrianto Lie <andrianto.lie@ii5.com>
2018-11-20 12:04:39 +0800

    Update lesson 2 to support eosio.cdt

--- eosio_docker/contracts/cardgame/cardgame.cpp
+++ eosio_docker/contracts/cardgame/cardgame.cpp
@@ -1,3 +1,16 @@
 #include "gameplay.cpp"
 
-EOSIO_DISPATCH(cardgame, BOOST_PP_SEQ_NIL)
+void cardgame::login(name username) {
+  // Ensure this action is authorized by the player
+  require_auth(username);
+  
+  // Create a record in the table if the player doesn't exist in our app yet
+  auto user_iterator = _users.find(username.value);
+  if (user_iterator == _users.end()) {
+    user_iterator = _users.emplace(username,  [&](auto& new_user) {
+      new_user.username = username;
+    });
+  } 
+}
+
+EOSIO_DISPATCH(cardgame, (login))
--- eosio_docker/contracts/cardgame/cardgame.hpp
+++ eosio_docker/contracts/cardgame/cardgame.hpp
@@ -4,8 +4,26 @@ using namespace std;
 using namespace eosio;
 class [[eosio::contract]] cardgame : public eosio::contract {
 
+  private:
+
+    struct [[eosio::table]] user_info {
+      name            username;
+      uint16_t        win_count = 0;
+      uint16_t        lost_count = 0;
+
+      auto primary_key() const { return username.value; }
+    };
+
+    typedef eosio::multi_index<name("users"), user_info> users_table;
+
+    users_table _users;
+
   public:
 
-    cardgame( name receiver, name code, datastream<const char*> ds ):contract(receiver, code, ds) {}
+    cardgame( name receiver, name code, datastream<const char*> ds ):contract(receiver, code, ds),
+                       _users(receiver, receiver.value) {}
+
+    [[eosio::action]]
+    void login(name username);
 
 };
