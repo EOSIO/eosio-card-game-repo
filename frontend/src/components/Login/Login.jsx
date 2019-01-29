3e27ac7846234d943620e947483e635aa816ce1f
Akhil James <akhiljames@live.com>
2019-01-29 16:38:39 +0530

    eos wallet draft

--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -49,10 +49,6 @@ class Login extends Component {
 
   // Handle form submission to call api
   handleSubmit(event) {
-    // Stop the default form submit browser behaviour
-    event.preventDefault();
-    // Extract `form` state
-    const { form } = this.state;
     // Extract `setUser` of `UserAction` and `user.name` of UserReducer from redux
     const { setUser } = this.props;
     // Set loading spinner to the button
@@ -60,9 +56,9 @@ class Login extends Component {
     // Send a login transaction to the blockchain by calling the ApiService,
     // If it successes, save the username to redux store
     // Otherwise, save the error state for displaying the message
-    return ApiService.login(form)
-      .then(() => {
-        setUser({ name: form.username });
+    return ApiService.login()
+      .then((username) => {
+        setUser({ name: username });
       })
       .catch(err => {
         this.setState({ error: err.toString() });
@@ -82,39 +78,11 @@ class Login extends Component {
       <div className="Login">
         <div className="title">Elemental Battles - powered by EOSIO</div>
         <div className="description">Please use the Account Name and Private Key generated in the previous page to log into the game.</div>
-        <form name="form" onSubmit={ this.handleSubmit }>
-          <div className="field">
-            <label>Account name</label>
-            <input
-              type="text"
-              name="username"
-              value={ form.username }
-              placeholder="All small letters, a-z, 1-5 or dot, max 12 characters"
-              onChange={ this.handleChange }
-              pattern="[\.a-z1-5]{2,12}"
-              required
-            />
-          </div>
-          <div className="field">
-            <label>Private key</label>
-            <input
-              type="password"
-              name="key"
-              value={ form.key }
-              onChange={ this.handleChange }
-              pattern="^.{51,}$"
-              required
-            />
-          </div>
-          <div className="field form-error">
-            { error && <span className="error">{ error }</span> }
-          </div>
-          <div className="bottom">
-            <Button type="submit" className="green" loading={ isSigningIn }>
-              { "CONFIRM" }
-            </Button>
-          </div>
-        </form>
+        <div className="bottom">
+          <Button type="submit" className="green" loading={ isSigningIn } onClick={ this.handleSubmit }>
+            { "CONFIRM" }
+          </Button>
+        </div>
       </div>
     )
   }
--- frontend/src/services/ApiService.js
+++ frontend/src/services/ApiService.js
@@ -3,28 +3,15 @@ import JsSignatureProvider from 'eosjs/dist/eosjs-jssig'
 
 // Main action call to blockchain
 async function takeAction(action, dataValue) {
-  const privateKey = localStorage.getItem("cardgame_key");
-  const rpc = new JsonRpc(process.env.REACT_APP_EOS_HTTP_ENDPOINT);
-  const signatureProvider = new JsSignatureProvider([privateKey]);
-  const api = new Api({ rpc, signatureProvider, textDecoder: new TextDecoder(), textEncoder: new TextEncoder() });
-
-  // Main call to blockchain after setting action, account_name and data
   try {
-    const resultWithConfig = await api.transact({
-      actions: [{
-        account: process.env.REACT_APP_EOS_CONTRACT_NAME,
-        name: action,
-        authorization: [{
-          actor: localStorage.getItem("cardgame_account"),
-          permission: 'active',
-        }],
-        data: dataValue,
-      }]
-    }, {
-      blocksBehind: 3,
-      expireSeconds: 30,
-    });
-    return resultWithConfig;
+    var account_name = localStorage.getItem("cardgame_account");
+    if (account_name) {
+      var eosWallet = window.eosWallet;
+      var resultWithConfig = await eosWallet.sendCustomAction(process.env.REACT_APP_EOS_CONTRACT_NAME, action, dataValue);
+      return resultWithConfig;
+    } else {
+      throw new Error("Something went wrong");
+    }
   } catch (err) {
     throw(err)
   }
@@ -49,19 +36,25 @@ class ApiService {
     });
   }
 
-  static login({ username, key }) {
-    return new Promise((resolve, reject) => {
-      localStorage.setItem("cardgame_account", username);
-      localStorage.setItem("cardgame_key", key);
-      takeAction("login", { username: username })
-        .then(() => {
-          resolve();
-        })
-        .catch(err => {
-          localStorage.removeItem("cardgame_account");
-          localStorage.removeItem("cardgame_key");
-          reject(err);
-        });
+  static login() {
+    return new Promise(async (resolve, reject) => {
+      var eosWallet = window.eosWallet;
+      var { data, status } = await eosWallet.connect();
+      if (status == "success") {
+        var username = data["account_name"];
+        localStorage.setItem("cardgame_account", username);
+        takeAction("login", { username })
+          .then(() => {
+            resolve(username);
+          })
+          .catch(err => {
+            localStorage.removeItem("cardgame_account");
+            localStorage.removeItem("cardgame_key");
+            reject(err);
+          });
+      } else {
+        reject("No permission")
+      }
     });
   }
 
